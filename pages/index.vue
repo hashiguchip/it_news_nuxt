3457913f30e70dcb90b9bbd91426d39f8013bfdb
hashiguchip <hashiguchip@gmail.com>
2019-04-30 12:56:49 +0900

    [add] panel design reform

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     .main-contents__panels.panels-block
         a.panels-block__panel.panel(:href="item.url" target="_blank" v-for="item in items")
             .panel__text {{item.name}}
+            img.panel__image(:src="item.image")
+            //todo:favorite ハート
+            .panel__favorite(:class="favorite")
 </template>
 <script lang="ts">
 import { Vue } from "nuxt-property-decorator";
@@ -48,13 +51,33 @@ export default class extends Vue {
     align-items: stretch
     height: 400px
     &__panel
-        background-color: orangered
         width: 100%
         height: 100%
 .panel
-    display: flex
-    align-items: center
-    justify-content: center
+    position: relative
+    width: 100%
+    height: 100%
+    background-color: #3B8070
     &__text
+        position: absolute
+        top: 50%
+        left: 50%
+        transform: translate(-50%,-50%)
         font-size: 24px
+    &__image
+        z-index: -100
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        background: indianred
+        font-size: 24px
+    &__favorite
+        position: absolute
+        top: 20px
+        right: 20px
+        width: 50px
+        height: 50px
+        background: #cd1514
 </style>
