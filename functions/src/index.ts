2f70d6126841f8acf77ec63d01d8b34b07658880
hashiguchip <hashiguchip@gmail.com>
2019-05-12 02:16:16 +0900

    [fix] local error ....

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,71 @@
 import * as functions from "firebase-functions";
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+
 import * as Express from "express";
 const app = Express();
 
+import * as admin from "firebase-admin";
 import * as data from "./data.json";
 import * as category from "./category.json";
 
+admin.initializeApp();
+
+// import { Nuxt } from "nuxt";
+
+// const config = {
+//   dev: false,
+//   buildDir: "nuxt",
+//   build: {
+//     publicPath: "/"
+//   }
+// };
+
+// const nuxt = new Nuxt(config);
+
 app.get("/sites", (req: Express.Request, res: Express.Response) => {
+  res.set("Cache-Control", "public, max-age=600, s-maxage-1200");
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  return res.send(data);
+  res.send(data);
 });
 
 app.get("/category", (req: Express.Request, res: Express.Response) => {
+  res.set("Cache-Control", "public, max-age=600, s-maxage-1200");
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  return res.send(category);
+  res.send(category);
 });
 
-// Expose Express API as a single Cloud Function:
+app.get("*", (req: Express.Request, res: Express.Response) => {
+  res.set("Cache-Control", "public, max-age=600, s-maxage-1200");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.send(category);
+});
+
+// function handleRequest(req: Express.Request, res: Express.Response) {
+//   res.set("Cache-Control", "public, max-age=600, s-maxage-1200");
+//   // add variables to req to use them in Nuxt component
+//   res.send("aaaaaaaaaaaaaaaaaaa");
+//   // nuxt
+//   //   .renderRoute("/", { req })
+//   //   .then(result => {
+//   //     res.send(result.html);
+//   //   })
+//   //   .catch(e => {
+//   //     res.send(e);
+//   //   });
+// }
+
+// app.get("*", handleRequest);
+
 exports.app = functions.https.onRequest(app);
