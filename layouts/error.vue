25105baf7eef9eab0fd7fedf93f67c1c9aa47c67
hashiguchip <hashiguchip@gmail.com>
2019-06-16 23:24:07 +0900

    [add] error page & layout

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.error-contents
+    h1.title エラーが発生しました。
+    .description このページは存在しません。
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {
+  public static layout = "error_layout";
+}
+</script>
+
+<style scoped lang="sass">
+.error-contents
+    margin: 100px 200px
+    & > .title
+        font-size: 24px
+        font-weight: bold
+</style>
---
+++ layouts/error_layout.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+    .page-wrapper
+        Header.page-wrapper__header
+        .page-wrapper__main
+            nuxt
+        Footer.page-wrapper__footer
+</template>
+<script lang="ts">
+  import { Component, Vue } from "~/node_modules/vue-property-decorator";
+  import Hero from "~/components/Hero.vue";
+  import Header from "~/components/Header.vue";
+  import Menu from "~/components/Menu.vue";
+  import Footer from "~/components/Footer.vue";
+
+  @Component({ components: { Header, Footer, Hero, Menu } })
+  export default class extends Vue {
+    /**
+     * レイアウト
+     */
+    public layout() {
+      return "default";
+    }
+  }
+</script>
+<style scoped lang="scss">
+    .page-wrapper {
+        width: 100%;
+        min-height: 100vh;
+        &__header {
+            height: 70px;
+            background: #4153b8;
+            box-shadow: 0 1px 10px rgba(0, 0, 0, 0.4);
+        }
+        &__main {
+            padding: 0 20px;
+            min-height: 100px;
+        }
+        &__footer {
+            height: 170px;
+            background: #1a5220;
+        }
+    }
+</style>
